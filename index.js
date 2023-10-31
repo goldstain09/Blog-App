c2c29369c9b5168428909408e62b2d209703e836
Sujal Rajput <rajputsujal992@gmail.com>
2023-10-31 16:45:08 +0530

    successfully setup backend for blog app!

---
+++ index.js
@@ -0,0 +1,10 @@
+// const express = require('express');
+// const app = express();
+// const http = require('http');
+
+// const server = http.createServer(app);
+
+
+
+const express = require('express');
+const server = express();
